372da905a2b22770e3bb03761c65480554482643
Adriano Teles <adriano.telesc@gmail.com>
2025-08-31 08:50:12 -0300

    Remove unused imports

--- lib/ui/local_config_entrypoint.dart
+++ lib/ui/local_config_entrypoint.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:local_config/ui/l10n/local_config_localizations.dart';
