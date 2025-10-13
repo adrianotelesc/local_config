6731e28902c1b75e8608fa9ee1c51654b2d15402
Adriano Teles <adriano.telesc@gmail.com>
2025-10-13 20:10:38 -0300

    Refactor map extension

--- lib/common/extension/map_extension.dart
+++ lib/common/extension/map_extension.dart
@@ -1,13 +1,9 @@
 extension MapExtension<K, V> on Map<K, V> {
-  Map<K, V> whereKey(bool Function(K) test) {
-    return Map<K, V>.fromEntries(entries.where((entry) => test(entry.key)));
-  }
+  Map<K, V> whereKey(bool Function(K) test) =>
+      Map<K, V>.fromEntries(entries.where((entry) => test(entry.key)));
 
-  List<(K, V)> get records {
-    return entries.map((entry) => (entry.key, entry.value)).toList();
-  }
+  List<(K, V)> get asRecords =>
+      entries.map((entry) => (entry.key, entry.value)).toList();
 
-  bool anyValue(bool Function(V) test) {
-    return values.any((value) => test(value));
-  }
+  bool anyValue(bool Function(V) test) => values.any((value) => test(value));
 }
