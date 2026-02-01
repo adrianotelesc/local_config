94f033bf05e17fa5ff08b6c1603ec97518f95d58
Adriano Teles <adriano.telesc@gmail.com>
2026-02-01 08:33:34 -0300

    Rename spacing class

--- lib/src/ui/theme/local_config_spacing.dart
+++ lib/src/ui/theme/local_config_spacing.dart
@@ -1,4 +1,4 @@
-abstract final class LocalConfigSpacings {
+abstract final class LocalConfigSpacing {
   static const double small = 8.0;
   static const double medium = 16.0;
   static const double large = 24.0;
